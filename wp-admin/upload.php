f161536558ddafaa4bfc3877c9e20fdb84d2c848
Neelaveni <npanditi@gitam.in>
2023-05-18 11:28:09 +0530

    2nd commit

--- wp-admin/upload.php
+++ wp-admin/upload.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=https://neelaveni8.github.io/trial/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D17806&reauth=1">
+		<meta http-equiv="refresh" content="0;url=https://neelaveni8.github.io/trial/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D26760&reauth=1">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "https://neelaveni8.github.io/trial/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D17806&reauth=1";
+			window.location = "https://neelaveni8.github.io/trial/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D26760&reauth=1";
 		</script>
 
-		<p>You are being redirected to <a href="https://neelaveni8.github.io/trial/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D17806&reauth=1">https://neelaveni8.github.io/trial/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D17806&reauth=1</a></p>
+		<p>You are being redirected to <a href="https://neelaveni8.github.io/trial/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D26760&reauth=1">https://neelaveni8.github.io/trial/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fupload.php%3Fsimply_static_page%3D26760&reauth=1</a></p>
 	</body>
 </html>
